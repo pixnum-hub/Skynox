82a2e7b505b970cd992f895b31574ea0870a259b
Manik Roy <pixnum@gmail.com>
2026-01-03 09:54:39 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="skynox-v8";
+const CACHE="skynox-v9";
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
   self.skipWaiting();

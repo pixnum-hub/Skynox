ecc437f035735fdc96d991e2f4c97d6f82521dcb
Manik Roy <pixnum@gmail.com>
2026-01-03 10:09:58 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE="skynox-v9";
+const CACHE="skynox-v1";
 self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))
+  );
   self.skipWaiting();
 });
-self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(i=>i!==CACHE&&caches.delete(i)))));
-  self.clients.claim();
-});
 self.addEventListener("fetch",e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
 });
